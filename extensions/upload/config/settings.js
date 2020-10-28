1a922458fa11f4e25ff0c94d20129dd5c3e462ac
Dominik Jasek <jasekdominik@seznam.cz>
2020-10-28 17:40:25 +0100

    changed provider to aws-s3

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,9 +1,22 @@
 module.exports = {
-  "provider": "cloudinary",
+  "provider": "aws-s3",
   "providerOptions": 
   {
-    "cloud_name": process.env.CLOUDINARY_CLOUD_NAME,
-    "api_key": process.env.CLOUDINARY_API_KEY,
-    "api_secret": process.env.CLOUDINARY_API_SECRET
+    "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+    "secretAccessKey": process.env.AWS_ACCESS_SECRET,
+    "region": 'eu-central-1',
+    "params": {
+      "Bucket": 'skauttrebic.cz',
+    },
   }
 };
+
+// module.exports = {
+//   "provider": "cloudinary",
+//   "providerOptions": 
+//   {
+//     "cloud_name": process.env.CLOUDINARY_CLOUD_NAME,
+//     "api_key": process.env.CLOUDINARY_API_KEY,
+//     "api_secret": process.env.CLOUDINARY_API_SECRET
+//   }
+// };
