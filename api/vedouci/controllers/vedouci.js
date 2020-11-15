0bd3db174cb4e0a044aed3fee90ae318f63d8fa6
Dominik Jasek <jasekdominik@seznam.cz>
2020-11-15 22:25:24 +0100

    local database for development removed redundat collection Spravce change attribute in aktuality... obrazek->obrazky (used in text)

--- api/vedouci/controllers/vedouci.js
+++ api/vedouci/controllers/vedouci.js
@@ -11,30 +11,30 @@ require('dotenv').config()
 
 
 module.exports = {
-  async create(ctx) {
-    let entity;
-    if (ctx.is("multipart")) {
-      const { data, files } = parseMultipartData(ctx);
-      entity = await strapi.services.vedouci.create(data, { files });
-    } else {
-      entity = await strapi.services.vedouci.create(ctx.request.body);
-    }
-    try {
-        console.log(ctx.request.body);
-        const msg = {
-            // to: "jasekdominik@seznam.cz",
-            // from: "jasekdominik@seznam.cz",
-            to: "jasekdominik@seznam.cz",
-            from: "2.skaut.od@gmail.com",
-            subject: "test more",
-            text: `${ctx.request.body}`,
-            html: `Nejnovější vedoucí se jmenuje <strong>${ctx.request.body.jmeno}</strong> a jeho funkce je <strong>${ctx.request.body.funkce}</strong>`,
-        }
-    } catch (err) {
-      console.log(err.message);
-      return err;
-    }
+  // async create(ctx) {
+  //   let entity;
+  //   if (ctx.is("multipart")) {
+  //     const { data, files } = parseMultipartData(ctx);
+  //     entity = await strapi.services.vedouci.create(data, { files });
+  //   } else {
+  //     entity = await strapi.services.vedouci.create(ctx.request.body);
+  //   }
+  //   try {
+  //       console.log(ctx.request.body);
+  //       const msg = {
+  //           // to: "jasekdominik@seznam.cz",
+  //           // from: "jasekdominik@seznam.cz",
+  //           to: "jasekdominik@seznam.cz",
+  //           from: "2.skaut.od@gmail.com",
+  //           subject: "test more",
+  //           text: `${ctx.request.body}`,
+  //           html: `Nejnovější vedoucí se jmenuje <strong>${ctx.request.body.jmeno}</strong> a jeho funkce je <strong>${ctx.request.body.funkce}</strong>`,
+  //       }
+  //   } catch (err) {
+  //     console.log(err.message);
+  //     return err;
+  //   }
 
-    return sanitizeEntity(entity, { model: strapi.models.vedouci });
-  },
+  //   return sanitizeEntity(entity, { model: strapi.models.vedouci });
+  // },
 };
