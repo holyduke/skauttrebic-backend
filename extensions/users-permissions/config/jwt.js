1941bf21f1cf9330677a9f70d4a8cfd033a0c8a1
Dominik Jasek <jasekdominik@seznam.cz>
2020-12-08 19:58:53 +0100

    jwt expires in 10 years

--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,6 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'aae2871e-ea8e-4eb0-84f1-c1dc8e69dee8'
+  jwtSecret: process.env.JWT_SECRET || 'aae2871e-ea8e-4eb0-84f1-c1dc8e69dee8',
+  jwt: {
+    expiresIn: "3650d",
+  }
 };
